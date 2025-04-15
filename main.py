abd157ad4bd2c415e30a0f981c0a01fdc2efb727
K-Thavros <valentin.iemi68@gmail.com>
2025-04-14 19:45:27 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Header
 from fastapi.responses import JSONResponse
+import os
 
 app = FastAPI()
 
+NOTION_SECRET = os.getenv("NOTION_SECRET")
+
 @app.post("/")
-async def verify(request: Request):
+async def verify(request: Request, notion_secret: str = Header(None)):
+    if notion_secret != NOTION_SECRET:
+        return {"error": "Token de verificación no válido"}
+
     body = await request.json()
     challenge = body.get("challenge")
 
